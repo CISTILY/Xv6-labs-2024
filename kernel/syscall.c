4f47cd6db2db72e69a0e10cb1f7abec0951cba09
Phuc0210 <128725672+Phuc0210@users.noreply.github.com>
2024-03-08 17:39:10 +0700

    Update syscall.c merge syscall_names

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -131,6 +131,7 @@ static uint64 (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_sysinfo] "sysinfo",
 };
 
 void
@@ -150,8 +151,3 @@ syscall(void)
     p->trapframe->a0 = -1;
   }
 }
-
-const char *syscall_names[] = {
-[SYS_sysinfo] "sysinfo",
-};
-
